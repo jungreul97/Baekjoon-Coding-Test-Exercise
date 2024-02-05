50f4cbb4cab7227290e48cd68c2707b04075b0e3
DESKTOP-SERQG8D\jungr <jungreul97>
2024-02-05 20:29:30 +0900

    줄세우기

--- IT대기업코테모음/줄세우기.py
+++ IT대기업코테모음/줄세우기.py
@@ -6,5 +6,11 @@ arr = []
 for _ in range(t):
     arr.append(list(map(int,input().split())))
 
-while t!=0:
-      
+for n in range(t):
+    total = 0
+    for i in range(1,21):
+        for j in range(i+1,21):
+            if arr[n][i]>arr[n][j]:
+                arr[n][i],arr[n][j] = arr[n][j],arr[n][i]
+                total+=1
+    print(arr[n][0],total)
