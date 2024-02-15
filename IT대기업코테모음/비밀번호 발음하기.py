b1e9d608c819615c78374473850b3b688f712ed6
DESKTOP-SERQG8D\jungr <jungreul97>
2024-02-15 23:23:35 +0900

    4659 비밀번호 발음하기

--- IT대기업코테모음/비밀번호 발음하기.py
+++ IT대기업코테모음/비밀번호 발음하기.py
@@ -1,16 +1,40 @@
 import sys
 input = sys.stdin.readline
-
-check = ['a','e','i','o','u']
+mo = ['a','e','i','o','u']
 while True:
-    s = input().rstrip()
-    mo = 0
-    ja = 0
-    mo_check = True
-    s2 = ''
-    for c in s:
-        mo_check = False
-        s2.append(c)
-        if len(s2) == 2:
-            if s == 'ee' or s== 'oo':
-                mo_check = False
+    word = input().rstrip()
+    if word == 'end':
+        break
+    answer = True
+    mocheck = False
+    mo_cnt = 0
+    ja_cnt = 0
+    for i in range(len(word)):
+        if word[i] in mo:
+            mocheck  = True
+            mo_cnt+=1
+            ja_cnt = 0
+        else:
+            mo_cnt=0
+            ja_cnt+=1
+        if mo_cnt == 3 or ja_cnt == 3:
+            answer = False
+            break
+        if mo_cnt == 2 and word[i] == word[i-1] and (word[i-1:i+1] != 'ee' and word[i-1:i+1] != 'oo'):
+            answer = False
+            break
+        if ja_cnt == 2 and word[i] == word[i-1]:
+            answer = False
+            break
+    
+    if not answer or not mocheck : 
+        print("<{0}> is not acceptable.".format(word))
+    else:
+        print("<{0}> is acceptable.".format(word))
+
+
+    
+
+        
+                
+                
