114a3e51440c273e08dfffab0a3ab7aeba03002c
DESKTOP-SERQG8D\jungr <jungreul97>
2024-02-23 15:53:49 +0900

    2493 탑

--- 디지털하나로/완전탐색/14718 용감한 용사 진수.py
+++ 디지털하나로/완전탐색/14718 용감한 용사 진수.py
@@ -1,15 +1,24 @@
 import sys
 input = sys.stdin.readline
 
-n,k = map(int,input().split())
+n,m = map(int,input().split())
 warriors = []
-
+answer = int(1e9)
 for _ in range(n):
-    warriors.append(map(int,input().split()))
+    warriors.append(list(map(int,input().split())))
 
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            cnt = 0
+            a = warriors[i][0]
+            b = warriors[j][1]
+            c = warriors[k][2]
 
-    
-
-
+            for l in range(n):
+                if warriors[l][0]<=a and warriors[l][1]<=b and warriors[l][2]<=c:
+                    cnt+=1
+            if cnt>=m:
+                answer = min(answer,a+b+c)
 
 print(answer)
