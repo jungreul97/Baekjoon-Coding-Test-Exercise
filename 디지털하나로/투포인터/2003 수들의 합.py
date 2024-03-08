b6956142e0ee3c545d994d03ffcf62369a3aa1d0
DESKTOP-SERQG8D\jungr <jungreul97>
2024-03-08 15:39:36 +0900

    2003 수들의 합

---
+++ 디지털하나로/투포인터/2003 수들의 합.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+arr = list(map(int,input().split()))
+cnt = 0
+start,end = 0,1
+while start <= end :
+    print(start,end)
+    print(*arr[start : end ])
+    if sum(arr[start:end]) == M : 
+        cnt+=1
+        start += 1
+        end += 1
+    elif sum(arr[start:end]) > M :
+        start += 1
+    else:
+        end += 1
+    if end == N+1: 
+        start+=1
+        end-=1
+print(cnt)
