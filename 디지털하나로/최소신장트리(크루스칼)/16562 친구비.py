b39328224d31635cf08a626ad919073ce8978e3a
DESKTOP-SERQG8D\jungr <jungreul97>
2024-03-18 12:50:26 +0900

    최소신장트리 16562 친구비

---
+++ 디지털하나로/최소신장트리(크루스칼)/16562 친구비.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+def find_parent(parent,x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x]  
+
+def union_parent(parent,a,b):
+    a,b = find_parent(parent,a),find_parent(parent,b) # 부모 찾고
+    if a == b: return
+    if moneys[a]<moneys[b]  : 
+        parent[b] = a
+        moneys[b] = 0 # 부모노드가 아니면 비용을 0으로 만들기
+    else : 
+        parent[a] = b
+        moneys[a] = 0
+
+N,M,k = map(int,(input().split()))
+moneys = [0]+list(map(int,input().split()))
+edges = []
+parent = [i for i in range(N+1)]
+
+for _ in range(M):
+    a,b = map(int,input().split())
+    union_parent(parent,a,b)
+print(parent)
+print(moneys)
+total = sum(moneys)
+
+if k<total: print("Oh no")
+else : print(total)
